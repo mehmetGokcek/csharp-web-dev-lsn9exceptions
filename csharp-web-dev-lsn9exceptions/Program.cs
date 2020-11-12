86f719e3acbe6da67242a91a943eb725d2c9f18d
Mehmet Gokcek <gokcek50@gmail.com>
2020-11-11 22:18:16 -0600

    passed all tests

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,24 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        static double Divide(double x, double y)
+        public static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("cannot divide by zero");
+            }
+
+            else 
+            {
+                return x / y;
+            }
         }
 
-        static int CheckFileExtension(string fileName)
+        public static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (fileName == null | fileName.Length == 0)
+            {
+                throw new ArgumentNullException("Submitted file cannot be null or empty!");
+            }
+
+            else
+            {
+                if (fileName.EndsWith(".cs"))
+                {
+                    return 1;
+                }
+
+                else
+                {
+                    return 0;
+                }
+            }
         }
+      
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            Dictionary<double, double> numbers = new Dictionary<double, double>();
+            numbers.Add(2, 3);
+            numbers.Add(4, 2);
+            numbers.Add(10, 0);
+            numbers.Add(50, 5);
+
+            int count = 0;
+            foreach (KeyValuePair<double, double> number in numbers)
+            {
+                count++;
+                try
+                {
+                    double result = Divide(number.Key, number.Value);
+                    Console.WriteLine($"Result #{count}: {result}\n");
+                }
+
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine($"Result #{count}: {e}\n");
+                }
+            }
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -28,6 +76,20 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Stefanie", "CoolProgram.cs");
 
 
+            foreach (KeyValuePair<string, string> student in students)
+            {
+                try
+                {
+                    CheckFileExtension(student.Value);
+                }
+
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+
         }
-    }
+}
 }
